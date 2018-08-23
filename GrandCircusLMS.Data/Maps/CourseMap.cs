7a0bd76ec9ad22479f559defaf05873498346810
Brian Gardner <1761566+briangardner@users.noreply.github.com>
2018-08-22 22:13:07 -0400

    Add maps for Location and Course

---
+++ GrandCircusLMS.Data/Maps/CourseMap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrandCircusLMS.Domain.Models;
+
+namespace GrandCircusLMS.Data.Maps
+{
+    class CourseMap : EntityTypeConfiguration<Course>
+    {
+        public CourseMap()
+        {
+            HasKey(x => x.Id);
+            Property(x => x.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            HasMany(x => x.Enrollments)
+                .WithRequired(x => x.Course)
+                .HasForeignKey(x => x.CourseId);
+            HasMany(x => x.Instructors)
+                .WithMany(x => x.Courses)
+                .Map(map =>
+                {
+                    map.MapRightKey("CourseId");
+                    map.MapLeftKey("InstructorId");
+                    map.ToTable("CourseInstructors");
+                });
+
+        }
+    }
+}
---
+++ GrandCircusLMS.Data/Maps/LocationMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrandCircusLMS.Domain.Models;
+
+namespace GrandCircusLMS.Data.Maps
+{
+    class LocationMap : EntityTypeConfiguration<Location>
+    {
+        public LocationMap()
+        {
+            HasKey(x => x.Id);
+            Property(x => x.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            HasMany(x => x.Courses)
+                .WithRequired(x => x.Location)
+                .HasForeignKey(z =>z.LocationId);
+        }
+    }
+}
