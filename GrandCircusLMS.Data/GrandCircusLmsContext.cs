6b7fc24aca183992acae7db7bc123f302f8d3d98
Brian Gardner <1761566+briangardner@users.noreply.github.com>
2018-08-22 22:00:05 -0400

    Started adding our DbContext.

---
+++ GrandCircusLMS.Data/GrandCircusLmsContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrandCircusLMS.Domain.Models;
+
+namespace GrandCircusLMS.Data
+{
+    public class GrandCircusLmsContext : DbContext
+    {
+        public GrandCircusLmsContext(): base("GrandCircusLMS")
+        {
+            //Drop the database and recreate on each run
+            //Database.SetInitializer(new DropCreateDatabaseAlways<GrandCircusLmsContext>());
+            // Create the DB if it doesn't exist.  
+            //Database.SetInitializer(new CreateDatabaseIfNotExists<GrandCircusLmsContext>());
+            //Will Drop and recreate if model changes.
+            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<GrandCircusLmsContext>());
+            //Custom Initializer
+
+        }
+
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        
+    }
+}
---
+++ GrandCircusLMS.Data/GrandCircusLmsInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandCircusLMS.Data
+{
+    class GrandCircusLmsInitializer : CreateDatabaseIfNotExists<GrandCircusLmsContext>
+    {
+        protected override void Seed(GrandCircusLmsContext context)
+        {
+            //This is where we start to seed our data.
+            base.Seed(context);
+        }
+    }
+}
