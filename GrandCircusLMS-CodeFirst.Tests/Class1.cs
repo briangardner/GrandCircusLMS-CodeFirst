69b95b71313f3a81e40c69e999f9bccf4d5fb020
Brian Gardner <1761566+briangardner@users.noreply.github.com>
2018-08-25 14:46:21 -0400

    Added test projects.  Added Nunit, Added Moq.

---
+++ GrandCircusLMS-CodeFirst.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandCircusLMS_CodeFirst.Tests
+{
+    public class Class1
+    {
+    }
+}
