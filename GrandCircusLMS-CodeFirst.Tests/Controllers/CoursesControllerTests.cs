509edac5855393a7131f41590d756e13030a1ba1
Brian Gardner <1761566+briangardner@users.noreply.github.com>
2018-08-25 15:34:16 -0400

    Added unit testing to Service and Controller.

---
+++ GrandCircusLMS-CodeFirst.Tests/Controllers/CoursesControllerTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using GrandCircusLMS.Data.Interfaces;
+using GrandCircusLMS.Domain.Interfaces.Services;
+using GrandCircusLMS.Domain.Models;
+using GrandCircusLMS_CodeFirst.Controllers;
+using GrandCircusLMS_CodeFirst.Models.Courses;
+using Moq;
+using NUnit.Framework;
+
+namespace GrandCircusLMS_CodeFirst.Tests.Controllers
+{
+    [TestFixture]
+    public class CoursesControllerTests
+    {
+        private readonly List<Student> _passingStudents = new List<Student>()
+        {
+            new Student()
+            {
+                FirstName = "Bobby",
+                LastName = "Tables",
+                DeclaredMajor = "Database Security"
+            }
+        };
+
+        private readonly IQueryable<Course> _courses = new List<Course>()
+        {
+            new Course()
+            {
+                Id = 5,
+                Name = "Test Course",
+                Instructors = new List<Instructor>(),
+                Location = new Location()
+                {
+                    Name = "Test Location"
+                },
+                ProgramManager = new ProgramManager()
+                {
+                    FirstName = "Test",
+                    LastName = "Program Manager"
+                }
+            }
+        }.AsQueryable();
+
+        private Mock<IGrandCircusLmsContext> _contextMock;
+        private Mock<ICourseService> _courseServiceMock;
+        private Mock<IDbSet<Course>> _courseSetMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _courseServiceMock = new Mock<ICourseService>();
+            _courseServiceMock.Setup(x => x.GetStudentsPassing(It.IsAny<Course>())).Returns(_passingStudents);
+
+            _courseSetMock = new Mock<IDbSet<Course>>();
+            _courseSetMock.As<IQueryable<Course>>().Setup(m => m.Provider).Returns(_courses.Provider);
+            _courseSetMock.As<IQueryable<Course>>().Setup(m => m.Expression).Returns(_courses.Expression);
+            _courseSetMock.As<IQueryable<Course>>().Setup(m => m.ElementType).Returns(_courses.ElementType);
+            _courseSetMock.As<IQueryable<Course>>().Setup(m => m.GetEnumerator()).Returns(_courses.GetEnumerator);
+            _courseSetMock.Setup(x => x.Find(It.IsAny<int>())).Returns(_courses.First);
+            
+
+            _contextMock = new Mock<IGrandCircusLmsContext>();
+            _contextMock.Setup(x => x.Courses).Returns(_courseSetMock.Object);
+        }
+
+        [Test]
+        public void Should_Return_ViewModel_With_CourseDetails()
+        {
+            var controller = new CoursesController(_contextMock.Object, _courseServiceMock.Object);
+            var result = (controller.CourseDetails(5) as ViewResult)?.ViewData.Model as CourseDetailsViewModel;
+
+            Assert.AreEqual(_passingStudents, result?.StudentsPassing);
+            
+        }
+    }
+}
---
+++ GrandCircusLMS.Infrastructure.Tests/Services/CourseServiceTests.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using GrandCircusLMS.Data.Interfaces;
+using GrandCircusLMS.Domain.Enums;
+using GrandCircusLMS.Domain.Models;
+using GrandCircusLMS.Infrastructure.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace GrandCircusLMS.Infrastructure.Tests.Services
+{
+    [TestFixture]
+    public class CourseServiceTests
+    {
+        private readonly IQueryable<Enrollment> _enrollments = new List<Enrollment>()
+        {
+            new Enrollment()
+            {
+                CourseId = 1,
+                Grade = Grade.F,
+                Student = new Student()
+                {
+                    FirstName = "Failing",
+                    LastName = "Student"
+                }
+            },
+            new Enrollment()
+            {
+                CourseId = 1,
+                Grade = Grade.B,
+                Student = new Student()
+                {
+                    FirstName = "Passing",
+                    LastName = "Student"
+                }
+            },
+            new Enrollment()
+            {
+                CourseId = 1,
+                Student = new Student()
+                {
+                    FirstName = "Missing Grade",
+                    LastName = "Student"
+                }
+            }
+        }.AsQueryable();
+
+        private readonly Course testCourse = new Course()
+        {
+            Id = 1,
+            Credits = 1,
+            LocationId = 1,
+            ProgramManagerId = 1
+        };
+        
+        private readonly Mock<IDbSet<Enrollment>> _mockEnrollments = new Mock<IDbSet<Enrollment>>();
+        private Mock<IGrandCircusLmsContext> _contextMock;
+        
+
+        [SetUp]
+        public void Setup()
+        {
+            
+
+            _mockEnrollments.As<IQueryable<Enrollment>>().Setup(m => m.Provider).Returns(_enrollments.Provider);
+            _mockEnrollments.As<IQueryable<Enrollment>>().Setup(m => m.Expression).Returns(_enrollments.Expression);
+            _mockEnrollments.As<IQueryable<Enrollment>>().Setup(m => m.ElementType).Returns(_enrollments.ElementType);
+            _mockEnrollments.As<IQueryable<Enrollment>>().Setup(m => m.GetEnumerator()).Returns(_enrollments.GetEnumerator);
+
+
+            _contextMock = new Mock<IGrandCircusLmsContext>();
+            _contextMock.Setup(x => x.Enrollments).Returns(_mockEnrollments.Object);
+        }
+
+        [Test]
+        public void Should_Return_Students_With_Passing_Grade()
+        {
+            var service = new CourseService(_contextMock.Object);
+            var passing = service.GetStudentsPassing(testCourse);
+            var student = passing.First();
+            Assert.AreEqual("Passing", student.FirstName);
+
+        }
+
+        [Test]
+        public void Should_Return_Students_With_Failing_Grade()
+        {
+            var service = new CourseService(_contextMock.Object);
+            var students = service.GetStudentsFailing(testCourse);
+            var student = students.First();
+            Assert.AreEqual("Failing", student.FirstName);
+
+        }
+
+        [Test]
+        public void Should_Return_Students_With_Missing_Grade()
+        {
+            var service = new CourseService(_contextMock.Object);
+            var students = service.GetStudentsWithoutGrade(testCourse);
+            var student = students.First();
+            Assert.AreEqual("Missing Grade", student.FirstName);
+
+        }
+    }
+}
